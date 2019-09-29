19b1abba67a1bbce2f092803170faa5a3e00329d
MahranShibly <41287438+mahranshibly@users.noreply.github.com>
2019-09-30 00:29:20 +0300

    Update ex4.sql

--- ex4.sql
+++ ex4.sql
@@ -6,11 +6,13 @@
 -- for each customer and for each year, the number of orders each month.
 -- The list will include customer number, year, number of orders in month 1,
 -- number of orders in month 2, and so on until 12, and total orders throughout the year.
--- The should be list sorted by customer name and year.
+-- The should be list sorted by customer name and year 
+-- (name ? with/without selecting it).
 
 SELECT	
 	(CASE WHEN t.CustomerID IS NULL THEN '_All-Customers_' ELSE t.CustomerID END) AS CustomerID ,
-	(CASE WHEN c.CompanyName IS NULL THEN '_All-Customers_' ELSE c.CompanyName END) AS CustomerName , 
+	--(CASE WHEN c.CompanyName IS NULL THEN '_All-Customers_' ELSE c.CompanyName END) AS CustomerName , 
+	-- ^^^ optional row-code (tested no exp.), to select CustomerName.
 	(CASE WHEN t.yyyyFormat IS NULL THEN 'All-Years' ELSE t.yyyyFormat END) OrdersYear,
 	SUM(CASE WHEN t.mmFormat = 01 THEN t.OrdersCount ELSE 0 END) AS Jan,
         SUM(CASE WHEN t.mmFormat = 02 THEN t.OrdersCount ELSE 0 END) AS Feb,
@@ -43,13 +45,11 @@ GROUP BY t.CustomerID, c.CompanyName, t.yyyyFormat WITH ROLLUP
 
 HAVING
 c.CompanyName IS NOT NULL  -- to remove all nulls or duplicated records (joining tabels).
-OR t.CustomerID IS NULL    -- my extra: to show all-customers  at all-years count per month as first row. 
+OR t.CustomerID IS NULL    -- my extra: to show all-customers  at all-years orders-count per month as first row. 
 
 ORDER BY c.CompanyName, OrdersYear; -- a list sorted by customer name and year!!!
 
 -- I wish my extra is clear...
--- the t.CustomerID is NULL just in one case, 
--- by grouping all orders as null-customerid (all), 
--- and null-year (all) and valid month (not null).
--- in this way we can get one more record(full row) in the list,
--- counting the orders per month for All-Customers at All-Years!!!.
+-- the t.CustomerID is NULL just in one case, by grouping all orders as null-id (all), 
+-- and null-year (all) and valid month (not null), in this way we can get one more record(full row) in the list,
+-- counting the orders per month for All-Customers at All-Years!!! as totals per month, very nice :)
